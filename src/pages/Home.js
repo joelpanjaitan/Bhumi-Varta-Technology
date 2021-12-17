f8591a7dd1a0ede74592f6744920aed538e470cf
joelpanjaitan <metrohal88@gmail.com>
2021-12-17 18:02:05 +0700

    this is the application

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,25 @@ function Home() {
     <>
       <Grid container spacing={2} alignItems="stretch">
         <Grid item sm={12} style={{ display: "flex" }} xs={12}>
-          <Navbar></Navbar>
+          <Grid sm={11} xs={4}>
+            <Navbar></Navbar>
+          </Grid>
+          <Grid sm={1} xs={8}>
+            <p>Photo</p>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <p>Left sidebar</p>
+        <Grid item xs={5} sm={12} style={{ display: "flex" }}>
+          <Grid ml={2} sm={2}>
+            <p>Left sidebar</p>
+          </Grid>
+          <Grid sm={8}>
+            <h1>Map content</h1>
+          </Grid>
+          <Grid sm={2}>
+            <p>Right sidebar</p>
+          </Grid>
         </Grid>
       </Grid>
-      <h1>this is home</h1>
     </>
   );
 }
